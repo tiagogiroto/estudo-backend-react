37e5eb8c8edc20e8724b901e735741bdce7692d4
Tiago Giroto Neto <50365633+tiagogiroto@users.noreply.github.com>
2024-09-02 14:08:12 +0000

    continuacao

---
+++ React/estudo/src/components/cardNames/cardNames.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+
+const cardNames = () => {
+
+    useEffect(() => {
+        
+    }, [])
+    
+
+  return (
+    
+    <Card>
+
+    </Card>
+    
+  )
+}
+
+export default cardNames;
